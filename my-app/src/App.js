284429ffd0e61d601128baa6c1d48feb1d4000c7
Jscastro197 <overbeatzme@gmail.com>
2023-05-19 16:28:32 -0400

    added page and route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom"
+import Dashboard from "./Pages/Dashboard"
 import Home from "./Pages/Home"
 import Login from "./Pages/Login"
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
