e4329a6fb3f2f3382604f8086151ca122b8eacc7
Jscastro197 <overbeatzme@gmail.com>
2023-05-20 17:28:43 -0400

    setting up db

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, request
-from pymongo import pymongo
+from flask import Flask
+from dotenv import dotenv_values
+from pymongo import MongoClient
 
+config = dotenv_values(".env")
 
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
 
-@app.route('/pdf')
-def pdf():
-    return 'Hello pdf!'
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+
+@app.teardown_appcontext
+def shutdown_db_client(exception=None):
+    app.mongodb_client.close()
