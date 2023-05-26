a035b967927102cec15e0746abb1f61d3b355ebc
Jeff Castro <133550490+Jscastro197@users.noreply.github.com>
2023-05-26 16:47:59 -0400

    bar

--- my-app/src/components/Piechart.js
+++ my-app/src/components/Piechart.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { Pie } from 'react-chartjs-2';
+import * as d3 from 'd3';
 
-const Piechart = () => {
-  const data = {
-    labels: ['Red', 'Blue', 'Yellow'],
-    datasets: [
-      {
-        data: [300, 50, 100],
-        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-      },
-    ],
-  };
-
+export const Piechart = () => {
+  const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+  
+  const w = 500;
+  const h = 100;
+  
   return (
     <div>
-      <Pie data={data} />
+      <svg width={w} height={h}>
+        {dataset.map((d, i) => (
+          <rect
+            key={i}
+            x={i * 30}
+            y={h - 3 * d}
+            width={25}
+            height={d * 3}
+            fill="navy"
+          />
+        ))}
+        {dataset.map((d, i) => (
+          <text
+            key={i}
+            x={i * 30}
+            y={h - 3 * d - 3}
+          >
+            {d}
+          </text>
+        ))}
+      </svg>
     </div>
   );
 };
 
-export default Piechart;
